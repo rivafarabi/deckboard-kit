bea28857a7957b1830bd6cb2889c55620b245de2
rivafarabi <rivafarabi.babeheer@gmail.com>
2019-07-07 22:30:27 +0700

    fix: delete utils from module.exports

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-"use strict";
+'use strict';
 const Extension = require('./Extension');
 const { INPUT_METHOD, PLATFORMS } = require('./constants');
-const utils, { log, prompt } = require('./utils');
+const { log, prompt } = require('./utils');
 
 module.exports = {
-  Extension,
-  utils,
-  prompt,
-  log,
-  INPUT_METHOD,
-  PLATFORMS
+	Extension,
+	prompt,
+	log,
+	INPUT_METHOD,
+	PLATFORMS
 };
