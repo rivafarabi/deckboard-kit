f5b17e2b0f89364930c73ae8b8389fd6c668f973
rivafarabi <rivafarabi.babeheer@gmail.com>
2019-07-07 22:26:17 +0700

    feat: add utils

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 "use strict";
 const Extension = require('./Extension');
 const { INPUT_METHOD, PLATFORMS } = require('./constants');
-const { log } = require('./utils');
+const utils, { log, prompt } = require('./utils');
 
 module.exports = {
   Extension,
+  utils,
+  prompt,
   log,
   INPUT_METHOD,
   PLATFORMS
