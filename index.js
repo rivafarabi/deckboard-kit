03f8d718de411484e4b19ee5919748b3b829a35d
rivafarabi <rivafarabi.babeheer@gmail.com>
2019-07-07 21:53:05 +0700

    fix: remove esm from devDependencies

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-require = require('esm')(module);
-module.exports = require('./main');
+"use strict";
+const Extension = require('./Extension');
+const { INPUT_METHOD, PLATFORMS } = require('./constants');
+const { log } = require('./utils');
+
+module.exports = {
+  Extension,
+  log,
+  INPUT_METHOD,
+  PLATFORMS
+};
